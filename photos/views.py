43099066bf71a03b74d3c189c02870dc74b547ce
Brian Otieno <bryomajor@gmail.com>
2019-11-10 17:09:37 +0300

    Define get_location and the get_category views

--- photos/views.py
+++ photos/views.py
@@ -5,7 +5,8 @@ from .models import Image
 # Create your views here.
 def my_photos(request):
     photos = Image.show_all_photos()
-    return render(request, 'all-photos/photos.html', {"photos":photos})
+    # category_results = category.objects.all()
+    return render(request, 'all-photos/photos.html', {"photos":photos,})
 
 def search_results(request):
 
@@ -19,3 +20,16 @@ def search_results(request):
     else:
         message = "You haven't searched for any term"
         return render(request, 'all-photos/search.html', {"message":message})
+
+
+def get_category(request, category):
+    category_results = category.objects.all()
+    location_results = Location.objects.all()
+    category_result = Image.objects.filter(category__category_name = category)
+    return render(request,'all-photos/photos.html',{'all_images':category_result,'category_results':category_results,'location_results':location_results})
+
+def get_location(request):
+    category_results = category.objects.all()
+    location_results = Location.objects.all()
+    location_result = Image.objects.filter(location__location_name= location)
+    return render(request,'all-photos/photos.html',{'all_images':location_result,'category_results':category_results,'location_results':location_results})
