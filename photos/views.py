ccf974d6b06368e4087a368c2ba3d37c60ae2e3f
Brian Otieno <bryomajor@gmail.com>
2019-11-11 15:05:10 +0300

    Rectify the search by category and get by location views

--- photos/views.py
+++ photos/views.py
@@ -1,21 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Image
+from .models import *
 
 # Create your views here.
 def my_photos(request):
     photos = Image.show_all_photos()
-    # category_results = category.objects.all()
-    return render(request, 'all-photos/photos.html', {"photos":photos,})
+    locations = Location.objects.all()
+    return render(request, 'all-photos/photos.html', {"photos":photos, "locations":locations})
 
 def search_results(request):
+    if 'search' in request.GET and request.GET["search"]:
+        category = request.GET.get("search")
+        searched_photos = Image.search_photo_by_category(category)
+        locations = Location.objects.all()
+        message = f"{category}"
 
-    if 'photo' in request.GET and request.GET["photo"]:
-        search_term = request.GET.get("photo")
-        searched_photos = Image.search_photo_by_category(search_term)
-        message = f"{search_term}"
-
-        return render(request, 'all-photos/search.html', {"message":message, "photos":searched_photos})
+        return render(request, 'all-photos/search.html', {"message":message, "photos":searched_photos, "locations":locations})
 
     else:
         message = "You haven't searched for any term"
@@ -28,8 +28,8 @@ def get_category(request, category):
     category_result = Image.objects.filter(category__category_name = category)
     return render(request,'all-photos/photos.html',{'all_images':category_result,'category_results':category_results,'location_results':location_results})
 
-def get_location(request):
+def get_location(request, location_name):
     category_results = category.objects.all()
-    location_results = Location.objects.all()
-    location_result = Image.objects.filter(location__location_name= location)
-    return render(request,'all-photos/photos.html',{'all_images':location_result,'category_results':category_results,'location_results':location_results})
+    locations = Location.objects.all()
+    location_result = Image.objects.filter(location__id= location_name)
+    return render(request,'all-photos/locations.html',{'all_images':location_result,'category_results':category_results,'locations':locations})
