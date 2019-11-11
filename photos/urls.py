f6848e37aaf3f9fe6887f1e0cc4ff9b35e06000a
Brian Otieno <bryomajor@gmail.com>
2019-11-11 15:05:28 +0300

    Rectify the url patterns

--- photos/urls.py
+++ photos/urls.py
@@ -6,8 +6,8 @@ from . import views
 urlpatterns=[
     url(r'^$', views.my_photos, name='myPhotos'),
     url(r'^search/', views.search_results, name='search_results'),
-    url(r'^category/(\w+)', views.get_category, name = 'get_category'),
-    url(r'^location/(\w+)', views.get_location, name = 'get_location')
+    url(r'^category/(\d+)', views.get_category, name = 'get_category'),
+    url(r'^location/(\d+)', views.get_location, name = 'get_location')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
